dae3861ed673b85e5d62fe5e605061ac8cc8ea33
lokendra1704 <lokendrabairwa@gmail.com>
2020-06-22 03:42:07 +0530

    Day4

---
+++ 1481_LeastNumberOfUniqueIntegers.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        c = Counter(arr)
+        sfreqs = sorted(c.values())
+        ans = len(sfreqs)
+        for freq in sfreqs:
+            if k>=freq:
+                k-=freq
+                ans-=1
+        return ans
+
+#Solution with heaps
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        c = Counter(arr)
+        heap = []
+        for a, b in c.items():
+            heapq.heappush(heap, [b, a])
+        while k > 0:
+            b, a = heapq.heappop(heap)
+            if b > 1:
+                heapq.heappush(heap, [b - 1, a])
+            k -= 1
+        return len(heap)
