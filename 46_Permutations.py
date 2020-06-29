13bced863d499447333e59109a602e0961f3da69
lokendra1704 <lokendrabairwa@gmail.com>
2020-06-30 01:35:21 +0530

    Day8

---
+++ 46_Permutations.py
@@ -0,0 +1,35 @@
+class Solution:
+    def permute(self, nums):
+        l = []
+        def perm(s, left, right):
+            if left == right:
+                l.append(s.copy())
+                return
+            for i in range(left, right):
+                s[left], s[i] = s[i], s[left]
+                perm(s, i+1, right)
+                s[left], s[i] = s[i], s[left]
+        perm(nums, 0, len(nums))
+        return l
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ret = []
+
+        def helper(temp):
+            if len(temp) == len(nums):
+                ret.append(temp + [])
+                return
+            for i in nums:
+                if i not in temp:
+                    temp.append(i)
+                    helper(temp)
+                    temp.pop()
+
+        helper([])
+        return ret
+
+
+x = Solution().permute(['a','b','c'])
+print(x)
