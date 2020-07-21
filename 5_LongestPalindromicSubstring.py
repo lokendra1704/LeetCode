736af4d2e6f99c2783ca3d7ebcf62f5eb3ffaf83
lokendra1704 <lokendrabairwa@gmail.com>
2020-07-21 23:37:17 +0530

    documentation

--- 5_LongestPalindromicSubstring.py
+++ 5_LongestPalindromicSubstring.py
@@ -1,3 +1,24 @@
+#Difficulty = Medium
+#Submission Speed = 48.40%
+'''
+Before Jumping onto Solution, know that:
+=>Palindromes are of two types: Odd Palindromes (length is odd) and Even Palindrome (length is even)
+=>We traverse the array, and for every element, we suppose that current element is the middle element and check for the largest palindromic
+  substring that can be made which has middle element as the current element.
+Note that, In case of even palindrome, there are two middle elements.
+=> We check for both Odd and even Palindromes
+=> How to check for largest palindromic substring with given middle element?
+    we maintain two pointers left and right.
+    Initialize them with current index (middle element index) and In every loop we check s[left]==s[right] (and check for 
+    boundary errors too, left and right dont go out of the size of array or 0). In the body of the loop we decrease the left
+    and increase the right pointer.
+'''
+'''
+Time Complexity =  O(N^2)
+Space Complexity = O(N)
+'''
+
+
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         longest = ""
